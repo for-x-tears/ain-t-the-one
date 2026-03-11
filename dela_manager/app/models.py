23cb7bef311d02a523a34976127f410d22ecdfd7
Nazar Berest <berestnazar25@gmail.com>
2026-03-11 23:48:55 +0300

    Update dela_manager

--- dela_manager/app/models.py
+++ dela_manager/app/models.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field
 from typing import Optional
 from uuid import UUID
 
-
 class Task(BaseModel):
     id: Optional[UUID] = None
-    title: str
-    description: Optional[str] = None
+    title: str = Field(..., min_length=8, max_length=50)
+    description: Optional[str] = Field(None, max_length=200)
     completed: bool = False
     time: Optional[str] = None
     #я пока что ещё не знаю какой переменной может быть время для выполнения и какое это вообще время
